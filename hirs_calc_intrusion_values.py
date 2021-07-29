3779fc99127f86b5660d85b3d7770b3dbaf3418e
Maximilian Ringel <maximilian.ringel@studium.uni-hamburg.de>
2021-07-29 14:19:48 +0200

    Minor script adjustemnts: Local saving, Code documentation etc.

--- hirs_calc_intrusion_values.py
+++ hirs_calc_intrusion_values.py
@@ -1,18 +1,30 @@
 import warnings
+# ignore some unimportant warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings(action='ignore', 
+message='The unit of the quantity is stripped when downcasting to ndarray.')
 
 import numpy as np
 import pandas as pd
 import hirs_src as src
 import sys
 
-print('**********')
+print('\n**********')
 
-# read in the filepath of the file with the suspected intrusion
-# given by the argument in the command line
+# read in the filepath (input-filename) of the file with the 
+# suspected intrusion given by the argument in the command line
 intrusion_file = sys.argv[1]
 
-# read satellite name from file path
+# retrieve date of the intrusion out of the input-filename 
+# string to use it in the output-filename
+date_of_intrusion = (
+    f'{intrusion_file[40:44]}-'
+    f'{intrusion_file[45:47]}-'
+    f'{intrusion_file[48:50]}'
+)
+print('Date of intrusion: ',date_of_intrusion)
+
+# retrieve satellite name from input-filename 
 SATELLITE = str(intrusion_file[28:34])
 print(f'\nSatellite: {SATELLITE}')
 
@@ -22,11 +34,12 @@ CHANNELS = np.arange(1,20)
 # set up hirs data handler
 read_hirs = src.get_hirs_reader(SATELLITE)
 
-# read in hirs file of moon intrusion
+# read in hirs-file of moon intrusion as xarray dataset
 (lines, extra) = read_hirs.read(intrusion_file)
 ds = read_hirs.as_xarray_dataset(lines)
 
-# read in the file with the channel-specified moon intrusion scan ranges
+# read in the file with the channel-specified moon intrusion scan ranges.
+# Raise error if file not found and exit.
 try:
     intr_scan_ranges = pd.read_csv('config_intr_scan_ranges.txt').set_index('channel')
 except FileNotFoundError:
@@ -58,9 +71,11 @@ for j in range(0,len(CHANNELS)):
         scanpos=range(10,56),
         channel=CHANNELS[j])
 
-    # find index of scanline with minimum mean counts
+    # find index and timestamp of scanline with minimum mean dsv counts
     # (= scnanline of moon intrusion)
     intrusion_timeindex = counts_scanlines.mean(dim="scanpos").argmin()
+    intrusion_timestamp = str(counts_scanlines.isel(
+        time=intrusion_timeindex).time.values)[11:19]
 
     # read in the prescriped range of the moon intrusion
     intrusion_range = [
@@ -69,6 +84,7 @@ for j in range(0,len(CHANNELS)):
         ]
 
     ### calculation of counts ###
+
     dsv_counts_mean, dsv_counts_std = src.calc_mean_counts_dsv(
         counts_scanlines,intrusion_timeindex)
     moon_counts_mean, moon_counts_std = src.calc_mean_counts(
@@ -77,6 +93,7 @@ for j in range(0,len(CHANNELS)):
         counts_scanlines_bb,intrusion_timeindex)
 
     #### calculation of blackbody temperature ###
+
     # number of blackbody temperature sensors
     nr_bb_sensors = len(ds.prt_number_iwt.values)
 
@@ -95,9 +112,9 @@ for j in range(0,len(CHANNELS)):
     ### calculation of blackbody radiance ###
         # try to read in txt file with meta information for 
         # the satellite and channel, to calculate the blackbody radiance.
-        # If no meta file is found, raise error message. 
+        # If no meta file is found, raise error message but continue with loop.
     try:
-        sat_meta_data = pd.read_csv(f'meta_{SATELLITE}.txt').set_index('channel')
+        sat_meta_data = pd.read_csv(f'meta_files/meta_{SATELLITE}.txt', comment='#').set_index('channel')
 
         ch_wavenumber = sat_meta_data.loc[CHANNELS[j]]['wavenumber']
         corr_fac1 = sat_meta_data.loc[CHANNELS[j]]['correction_factor1']
@@ -106,26 +123,19 @@ for j in range(0,len(CHANNELS)):
         bb_radiance = src.calc_bb_flux(ch_wavenumber*10**2,\
         [corr_fac1,corr_fac2],bb_temp_mean)
 
-        bb_rad_all.append(bb_radiance)
+        bb_rad_all.append(round(bb_radiance,2))
 
     except FileNotFoundError:
         print(f'\n*** No meta data file for {SATELLITE} found ***\n'
         '*** --> Blackbody radiance could not be calculated ***')
         continue
 
-# get platform information about longitude and altitude
+# get 3D position of the satellite
 longitude, latitude, altitude = src.get_position(ds,intrusion_timeindex)
 
-### save output to csv file ###
-date_of_intrusion = (
-    f'{intrusion_file[40:44]}-'
-    f'{intrusion_file[45:47]}-'
-    f'{intrusion_file[48:50]}'
-)
+### Save data to csv file ###
 
-PATH = 'moon_intrusion_calculations/'
-FILENAME = f'hirs_moon_intrusion_{SATELLITE}_{date_of_intrusion}_calculations.csv'
-
+# build dictionary with calculated data
 data = {
     'channel': CHANNELS,
     'intrusion scan-range': intrusion_range_all,
@@ -137,13 +147,26 @@ data = {
     'dsv counts std': dsv_counts_std_all,
     'bb counts mean': bb_counts_mean_all,
     'bb counts std': bb_counts_std_all,
-    'bb rad': bb_rad_all,
     'longitude': longitude.tolist(),
     'latitude': latitude.tolist(),
     'altitude': altitude.tolist(),
 }
 
-df = pd.DataFrame(data).set_index('channel')
+# convert dictionary to panda dataframe
+df = pd.DataFrame(data)
+
+# if blackbody radiance could be calculated  
+# insert the data into the dataframe at column 10 
+if len(bb_rad_all) != 0:
+    df.insert(10, "bb_rad", bb_rad_all, True)
+
+# set path to which the output csv file should be saved to
+# default path: local dircetory
+PATH = ''
+FILENAME = f'hirs_moon_intrusion_{SATELLITE}_{date_of_intrusion}_{intrusion_timestamp}_calculations.csv'
+
+# convert dataframe to csv and save it 
+df = df.set_index('channel')
 df.to_csv(PATH+FILENAME)
 
 print('')
@@ -154,5 +177,6 @@ print('')
 print('****************')
 print('All calculations finished!')
 print(f'Output saved to:    {PATH+FILENAME}')
+print('')
 
 #EOF
--- hirs_plot_all_channels.py
+++ hirs_plot_all_channels.py
@@ -1,10 +1,11 @@
 import warnings
+# ignore some unimportant warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings(action='ignore', 
+message='The unit of the quantity is stripped when downcasting to ndarray.')
 
 import numpy as np
 import pandas as pd
-import typhon
-import typhon.datasets.tovs
 from typhon.plots import styles
 import matplotlib.pyplot as plt
 import sys
@@ -12,8 +13,14 @@ import hirs_src as src
 
 styles.use('typhon')
 
+print('\n**********')
+
+# read in the filepath (input-filename) of the file with the 
+# suspected intrusion given by the argument in the command line
 intrusion_file = sys.argv[1]
 
+# read in the file with the channel-specified moon intrusion scan ranges.
+# Raise error if file not found and exit.
 try:
     intr_scan_ranges = pd.read_csv('config_intr_scan_ranges.txt').set_index('channel')
 except FileNotFoundError:
@@ -25,17 +32,23 @@ except FileNotFoundError:
 
 # set up data handler for hirs 
 SATELLITE = str(intrusion_file[28:34])
+print('\nSatellite: ',SATELLITE)
 read_hirs = src.get_hirs_reader(SATELLITE)
 
-# read in hirs file of moon intrusion
+
+# read in hirs file of moon intrusion as xarray dataset
 (lines, extra) = read_hirs.read(intrusion_file)
 ds = read_hirs.as_xarray_dataset(lines)
 
+# set all the text in plot to size 10 
 plt.rcParams['font.size'] = '10'
 
-# plotting
+# plotting loop:
+# plots a 5x4 matrix of plots each containing the dsv counts
+# for the individual channel during the intrusion.
 (fig, axs) = plt.subplots(nrows=5,ncols=4,sharex=True,figsize=(14,12))
 
+# this loop plots channels 1-5
 for i in range(0,5):
     
     channels = np.arange(1,6)
@@ -48,15 +61,22 @@ for i in range(0,5):
     timeindex_of_intrusion = counts.mean(dim="scanpos").argmin()
     scanline_of_intrusion_counts = counts.isel(time=timeindex_of_intrusion)
     
-    # plot counts measured during intrusion by channel i:
+    # retrieve the scan time of the intrusion to use it in the 
+    # plot title and filename
+    scan_time = str(scanline_of_intrusion_counts.time.values)[11:19]
+
+    # plot dsv counts measured during intrusion by channel i:
     axs[i,0].grid(axis='x',color='grey',linestyle='--',linewidth=0.4)
     axs[i,0].plot(scanline_of_intrusion_counts.scanpos,
                 scanline_of_intrusion_counts)
     axs[i,0].set_title(f'channel {channels[i]}\n'
-                       f'scan time: {str(scanline_of_intrusion_counts.time.values)[11:19]}',
+                       f'scan time: {scan_time}',
                        fontsize=8)
     axs[i,0].set_xlim(10,56)
 
+    # plot the lower and upper bound of the specified
+    # scan-range (specified in the 'config_intr_scan_ranges.txt' file) 
+    # of the intrusion as vertical lines
     axs[i,0].vlines(
         x=intr_scan_ranges.loc[channels[i]]['min_scanpos'],
         ymin=scanline_of_intrusion_counts.min(),
@@ -75,7 +95,8 @@ for i in range(0,5):
         linestyle='--',
         label='intrusion range',
         )
-    
+
+# this loop plots channels 6-10
 for i in range(0,5):
     
     channels = np.arange(6,11)
@@ -83,12 +104,9 @@ for i in range(0,5):
     counts = ds[{"time":ds["scantype"]==1}]["counts"].sel(
         scanpos=range(10,56),channel=channels[i])
     
-    # find scanline of intrusion by searching for the 
-    # scanline with the minimum average of counts:
     timeindex_of_intrusion = counts.mean(dim="scanpos").argmin()
     scanline_of_intrusion_counts = counts.isel(time=timeindex_of_intrusion)
     
-    # plot counts measured during intrusion by channel i:
     axs[i,1].grid(axis='x',color='grey',linestyle='--',linewidth=0.4)
     axs[i,1].plot(scanline_of_intrusion_counts.scanpos,
                 scanline_of_intrusion_counts)
@@ -116,6 +134,7 @@ for i in range(0,5):
         label='intrusion range',
         )
     
+# this loop plots channels 11-15
 for i in range(0,5):
     
     channels = np.arange(11,16)
@@ -123,12 +142,9 @@ for i in range(0,5):
     counts = ds[{"time":ds["scantype"]==1}]["counts"].sel(
         scanpos=range(10,56),channel=channels[i])
     
-    # find scanline of intrusion by searching for the 
-    # scanline with the minimum average of counts:
     timeindex_of_intrusion = counts.mean(dim="scanpos").argmin()
     scanline_of_intrusion_counts = counts.isel(time=timeindex_of_intrusion)
     
-    # plot counts measured during intrusion by channel i:
     axs[i,2].grid(axis='x',color='grey',linestyle='--',linewidth=0.4)
     axs[i,2].plot(scanline_of_intrusion_counts.scanpos,
                 scanline_of_intrusion_counts)
@@ -155,20 +171,19 @@ for i in range(0,5):
         linestyle='--',
         label='intrusion range',
         )
-    
+
+# this loop plots channels 16-19
+# (last plot is empty) 
 for i in range(0,4):
-    
+
     channels = np.arange(16,20)
     
     counts = ds[{"time":ds["scantype"]==1}]["counts"].sel(
         scanpos=range(10,56),channel=channels[i])
     
-    # find scanline of intrusion by searching for the 
-    # scanline with the minimum average of counts:
     timeindex_of_intrusion = counts.mean(dim="scanpos").argmin()
     scanline_of_intrusion_counts = counts.isel(time=timeindex_of_intrusion)
     
-    # plot counts measured during intrusion by channel i:
     axs[i,3].grid(axis='x',color='grey',linestyle='--',linewidth=0.4)
     axs[i,3].plot(scanline_of_intrusion_counts.scanpos,
                 scanline_of_intrusion_counts)
@@ -197,25 +212,32 @@ for i in range(0,4):
         )
     
     handles, labels = axs[i,3].get_legend_handles_labels()
-    
+
+# set general x- and y-labels for matrix plot as well as the label
 fig.text(0.5, -0.02, 'scanpos', ha='center',fontsize=13,fontweight="bold")
 fig.text(-0.02, 0.5, 'counts', va='center', rotation='vertical',fontsize=13,fontweight="bold")
-
 fig.legend(handles, labels, loc='lower right', prop={'size': 12})
 
-#axs[0,0].vlines(x=20,ymin=scanline_of_intrusion_counts.min(),ymax=scanline_of_intrusion_counts.max())
+# retrieve the date of the intrusion out of the timestamp 
+# in the data to use it in the plot title and output-filename
+date_of_intrusion = str(scanline_of_intrusion_counts.time.values)[:10]
+print('Date of intrusion: ',date_of_intrusion)
 
-DATE_OF_INTRUSION = str(scanline_of_intrusion_counts.time.values)[:10]
-
-fig.suptitle(f"POSSIBLE MOON INTRUSION for {SATELLITE}, DATE: {DATE_OF_INTRUSION}\n"
+# set plot title
+fig.suptitle(f"POSSIBLE MOON INTRUSION for {SATELLITE}, DATE: {date_of_intrusion}\n"
              f"granule file: {intrusion_file}",fontsize=12)
 
-    
+# adjust spacing in between the plots   
 fig.tight_layout(pad=1.2,w_pad=0)
 
-PATH = f'moon_intrusion_plots/{SATELLITE}/possible_intrusions/'
-FILE = f'hirs_moon_intrusion_{SATELLITE}_{DATE_OF_INTRUSION}_allchannels_dsv.pdf'
+# set path to which the plot should be saved to
+# default path: local dircetory
+PATH = f''
+FILE = f'hirs_moon_intrusion_{SATELLITE}_{date_of_intrusion}_{scan_time}_allchannels_dsv.pdf'
 plt.savefig(PATH+FILE,bbox_inches='tight')
 
 print('\n**************')
 print('Plot saved to: ',PATH+FILE)
+print('')
+
+#EOF
